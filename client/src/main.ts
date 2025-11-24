59cc78ed808bb1e72481a7c8b73f9fcbb475af26
Jason Fischl <jason.fischl@gmail.com>
2025-11-23 18:10:41 -0800

    Debug: Log server URL

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,7 @@
 import { io, Socket } from 'socket.io-client';
 import { RoomDataPacket, Inventory, Player } from '../../shared/types';
 
+console.log("Connecting to server at:", import.meta.env.VITE_SERVER_URL || 'http://localhost:3000');
 const socket: Socket = io(import.meta.env.VITE_SERVER_URL || 'http://localhost:3000');
 
 const output = document.getElementById('output') as HTMLDivElement;
